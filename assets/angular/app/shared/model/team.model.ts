384173fdc6fc246d0df5ff5446af042154820f52
Chris McGroarty <iammarix@gmail.com>
2018-05-03 01:09:03 -0400

    add Team model

---
+++ assets/angular/app/shared/model/team.model.ts
@@ -0,0 +1,17 @@
+import {Division} from "./division.model";
+
+export interface Team {
+	id?: number;
+	name?: string;
+	division?: Division;
+	school?: string;
+	color?: string;
+	city?: string;
+	state?: string;
+	contactName?: string;
+	contactEmail?: string;
+	here?: boolean;
+	arrived?: DateTimeFormat;
+	numKids?: number;
+
+}
