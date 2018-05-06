37f88db62c7656b429df8944d016b5a9da21e254
Chris McGroarty <iammarix@gmail.com>
2018-05-05 21:34:05 -0400

    fix toolbar to top, add container to pad content below it

--- assets/angular/app/app.component.ts
+++ assets/angular/app/app.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
+import {MatSidenavContent} from "@angular/material";
+import {LayoutService} from "./core/services/layout.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	@ViewChild(MatSidenavContent,{read: ElementRef})
+	public sidenavContainer: ElementRef;
+
+	constructor(private layout:LayoutService){}
+
+	ngOnInit(){
+		this.layout.sidenavContainer = this.sidenavContainer;
+	}
 }
