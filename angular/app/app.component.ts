ec7291a9063ef019be89fbb641645099b668f093
Chris McGroarty <iammarix@gmail.com>
2018-05-08 05:16:30 -0400

    migrate sidenav out of app component. better media breakpoint detect.

--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -1,24 +1,15 @@
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatSidenavContent } from '@angular/material';
-import { Observable } from 'rxjs';
-import { LayoutService } from './core/services/layout.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component( {
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.scss' ],
-	encapsulation: ViewEncapsulation.None
+	styleUrls: [ './app.component.scss' ]
 } )
 export class AppComponent implements OnInit {
-	@ViewChild( MatSidenavContent, { read: ElementRef } )
-	public sidenavContainer: ElementRef;
-	isHandset: Observable<BreakpointState> = this.breakpointObserver.observe( Breakpoints.Handset );
 
-	constructor( private layout: LayoutService, private breakpointObserver: BreakpointObserver ) {
+	constructor() {
 	}
 
 	ngOnInit() {
-		this.layout.sidenavContainer = this.sidenavContainer;
 	}
 }
--- angular/app/core/components/sidebar/sidebar.component.ts
+++ angular/app/core/components/sidebar/sidebar.component.ts
@@ -1,20 +1,45 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { Observable, Subject } from 'rxjs/index';
+import { combineLatest, takeUntil } from 'rxjs/operators';
 
 @Component( {
-	selector: 'app-sidebar',
+	selector: 'hyper-sidebar',
 	templateUrl: './sidebar.component.html',
 	styleUrls: [ './sidebar.component.scss' ]
 } )
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
 	LOGO = require( './Logo_2015.svg' );
-	@Output() closeSidenav = new EventEmitter<boolean>();
+	isHandset: boolean;
+	@ViewChild( 'sidenav' ) sidenav: MatSidenav;
+	private unsubscribe$ = new Subject<void>();
+
 
 	constructor( private breakpointObserver: BreakpointObserver ) {
 	}
 
 	ngOnInit() {
+		this.breakpointObserver.observe( [
+			Breakpoints.Handset,
+			Breakpoints.TabletPortrait,
+			Breakpoints.WebPortrait
+		] ).pipe( takeUntil( this.unsubscribe$ ) )
+			.subscribe( breakpoint => {
+			this.isHandset = breakpoint.matches;
+		} );
+	}
+
+	ngOnDestroy() {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
+
+	close(): void {
+		if ( this.isHandset ) {
+			this.sidenav.close();
+		}
 	}
 
 
