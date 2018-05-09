d05ed568e46f57585a0c3c7afe15501e3b82c3a4
Chris McGroarty <iammarix@gmail.com>
2018-05-09 18:01:52 -0400

    add Score api to Sails

---
+++ api/models/Score.js
@@ -0,0 +1,54 @@
+/**
+ * Score.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+const Table = ['1A', '1B', '2A', '2B'];
+module.exports = {
+	primaryKey: 'id',
+	attributes: {
+
+		//  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+		//  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+		//  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+		id: {
+			type: 'number',
+			required: true,
+			unique: true,
+			isInteger: true,
+			min: 0
+		},
+		table_name: {
+			type: 'string',
+			isIn: Table,
+			required: true
+		},
+		total_score: {
+			type: 'number',
+			isInteger: true,
+			allowNull: true,
+			min: 0
+		},
+
+		//  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+		//  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+		//  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+		//  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+		//  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+		//  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+		match: {
+			model: 'match',
+			required: true
+		},
+		team: {
+			model: 'team',
+			required: true
+		}
+	},
+
+};
+
