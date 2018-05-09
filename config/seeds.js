1d38e188a868f32cfec56ad1cc0b15cc21444d13
Chris McGroarty <iammarix@gmail.com>
2018-05-09 18:05:05 -0400

    add seeding of data to Sails

---
+++ config/seeds.js
@@ -0,0 +1,37 @@
+/**
+ * Sails Seed Settings
+ * (sails.config.seeds)
+ *
+ * Configuration for the data seeding in Sails.
+ *
+ * For more information on configuration, check out:
+ * http://github.com/frostme/sails-seed
+ */
+module.exports.seeds = {
+	school: {
+		data: require('../.config/mocks/schools'),
+		active: true,
+		// priority: 0,
+		overwrite: true
+	},
+	team: {
+		data: require('../.config/mocks/teams'),
+		active: true,
+		// priority: 0,
+		overwrite: true
+	},
+	match: {
+		data: require('../.config/mocks/matches'),
+		active: true,
+		// priority: 0,
+		overwrite: true
+	},
+	score: {
+		data: require('../.config/mocks/scores'),
+		active: false,
+		// priority: 0,
+		overwrite: true
+	},
+
+
+};
