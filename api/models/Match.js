1bb7421103d214d121637116e31bec57466094f1
Chris McGroarty <iammarix@gmail.com>
2018-05-09 18:03:43 -0400

    add Match model attributes to Sails

--- api/models/Match.js
+++ api/models/Match.js
@@ -4,15 +4,38 @@
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
-
 module.exports = {
-
+	primaryKey: 'id',
 	attributes: {
-
 		//  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
 		//  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
 		//  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
+		id: {
+			type: 'number',
+			required: true,
+			unique: true,
+			isInteger: true,
+			min: 0
+		},
+		time: {
+			type: 'ref',
+			columnType: 'datetime'
+		},
+		type: {
+			type: 'string',
+			isIn:  [ 'practice', 'qualification', 'elimination', 'replay' ],
+			defaultsTo: 'practice'
+		},
+		round: {
+			type: 'number',
+			isInteger: true,
+			min: 0
+		},
+		status: {
+			type: 'string',
+			isIn: [ 'unplayed', 'playing', 'played' ],
+			defaultsTo: 'unplayed'
+		},
 
 		//  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
 		//  ║╣ ║║║╠╩╗║╣  ║║╚═╗
@@ -22,7 +45,10 @@ module.exports = {
 		//  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
 		//  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
 		//  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+		scores: {
+			collection: 'score',
+			via: 'match'
+		}
 	},
 
 };
