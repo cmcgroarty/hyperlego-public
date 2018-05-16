85a38b1df5067f1ab765902de0adeeaecd4bdd37
Chris McGroarty <iammarix@gmail.com>
2018-05-16 05:57:41 -0400

    add no-config for hyperconfig hook

---
+++ api/hooks/hyperconfig/index.js
@@ -0,0 +1,37 @@
+/**
+ * hyperconfig hook
+ *
+ * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
+ * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
+ */
+
+module.exports = function defineHyperConfigHook( sails ) {
+
+	return {
+
+		/**
+		 * Runs when a Sails app loads/lifts.
+		 *
+		 * @param {Function} done
+		 */
+		initialize: function ( done ) {
+
+			sails.log.info( 'Initializing custom hook (`hyperconfig`)' );
+
+			require( 'no-config' )( {
+				config: require( '../../../.config' )
+			} ).then(
+				function ( conf ) {
+					let db = conf.mysql.hyperlego;
+					sails.config.datastores.default = {
+						adapter: 'sails-mysql',
+						url: 'mysql://' + db.user + ':' + db.password + '@' + db.host
+						+ ( db.port ? ':' + db.port : '' ) + '/' + db.database
+					};
+				} );
+			return done();
+		}
+
+	};
+
+};
